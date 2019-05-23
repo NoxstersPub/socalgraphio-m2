b8d13d1d5e9e226b7f307303a8fb3828d022398b
Nian <cstardev@gmail.com>
2019-05-23 19:19:37 +0530

    API and Test connection work

--- app/code/Blackbox/Epace/Controller/Adminhtml/Index/Index.php
+++ app/code/Blackbox/Epace/Controller/Adminhtml/Index/Index.php
@@ -1,13 +1,29 @@
 <?php
 
 namespace Blackbox\Epace\Controller\Adminhtml\Index;
-
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\Controller\ResultFactory;
 
 class Index extends \Magento\Backend\App\Action
 {
+	
+   protected $_apihelper;
+
+	public function __construct(
+	   \Magento\Framework\App\Action\Context $context,
+	   \Blackbox\Epace\Helper\Api $helper
+	) {
+		 $this->_apihelper = $helper;
+		 parent::__construct($context);
+		
+	}
+
     public function execute()
     {
-       die('sdsds');
+    	 $api = $this->_apihelper;
+    	
+     	echo  '***'.$api->getUsername();
+       die(' -->controller');
 	}
 }
 
