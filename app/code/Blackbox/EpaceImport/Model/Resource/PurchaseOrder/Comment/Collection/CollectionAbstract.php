fca56a4253585ff2a4098f4366134530b8f0ba2f
starengineer <cstardev@gmail.com>
2019-07-16 00:58:56 -0400

    Upgrade EpaceImport model (add missing files)

---
+++ app/code/Blackbox/EpaceImport/Model/Resource/PurchaseOrder/Comment/Collection/CollectionAbstract.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Blackbox\EpaceImport\Model\Resource\PurchaseOrder\Comment\Collection;
+
+abstract class CollectionAbstract extends \Magento\Sales\Model\ResourceModel\Collection\AbstractCollection
+{
+
+    public function setParentFilter($parent)
+    {
+        if ($parent instanceof \Magento\Framework\Model\AbstractModel) {
+            $parent = $parent->getId();
+        }
+        return $this->addFieldToFilter('parent_id', $parent);
+    }
+
+    /**
+     * Adds filter to get only 'visible on front' comments
+     *
+     * @param int $flag
+     * @return Blackbox_EpaceImport_Model_Resource_PurchaseOrder_Comment_Collection_Abstract
+     */
+    public function addVisibleOnFrontFilter($flag = 1)
+    {
+        return $this->addFieldToFilter('is_visible_on_front', $flag);
+    }
+
+    /**
+     * Set created_at sort order
+     *
+     * @param string $direction
+     * @return Blackbox_EpaceImport_Model_Resource_PurchaseOrder_Comment_Collection_Abstract
+     */
+    public function setCreatedAtOrder($direction = 'desc')
+    {
+        return $this->setOrder('created_at', $direction);
+    }
+}
